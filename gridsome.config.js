3b0bf0abdc24e34943fabb2b22ed5a6b9e4051c2
Jan Prill <Jan.Prill@mspag.com>
2020-03-29 00:53:05 +0100

    wip

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: '',
+  siteName: 'Krisenhilfe',
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss',
